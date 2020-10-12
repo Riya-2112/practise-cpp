06808520284f974ba4e87391d1115dae7dd1c352
Riya-2112 <67411339+Riya-2112@users.noreply.github.com>
2020-10-12 20:15:02 +0530

    Update ASCII_Values.cpp

--- ASCII_Values.cpp
+++ ASCII_Values.cpp
@@ -3,24 +3,24 @@ using namespace std;
 
 int main()
 {
-    char ch;
+    char cr;
     int i;
     cout<<"For uppercase letters."<<endl;
     for(i=65;i<91;i++)
     {
-        ch = i;
-        cout<<ch<<": "<<i<<endl;
+        cr = i;
+        cout<<cr<<": "<<i<<endl;
     }
     cout<<"For lowercase letters."<<endl;
     for(i=97;i<123;i++)
     {
-        ch=i;
-        cout<<ch<<": "<<i<<endl;
+        cr = i;
+        cout<<cr<<": "<<i<<endl;
     }
     cout<<"For Numbers from 1 to 9"<<endl;
     for(i=48;i<58;i++)
     {
-        ch=i;
-        cout<<ch<<": "<<i<<endl;
+        cr = i;
+        cout<<cr<<": "<<i<<endl;
     }
 }
